3f0c3d4c5bbd057851a6a072910f8e2cd41bc173
Mikemnjovu <mikemnjovu@gmail.com>
2020-06-04 01:43:10 +0200

    change user api with api containing images

--- src/Shared/Card/Card.js
+++ src/Shared/Card/Card.js
@@ -3,37 +3,38 @@ import Api from "../../Api";
 
 const Card = () => {
   const [person, setPerson] = useState("");
+  const [persontest, setPresontest] = useState("");
   useEffect(async () => {
-    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const response = await fetch("https://randomuser.me/api/?results=10");
     const data = await response.json();
-    const items = data;
+    const items = data.results;
     setPerson(items);
   }, []);
+
   const photoList = {
     unsplash: "https://source.unsplash.com/daily",
     picsum: "https:/picsum.photos/200/200",
   };
-  console.log(person);
   return (
     <div className="card-columns ">
       {person
         ? person.map((item) => (
-            <div className="shadow ">
+            <div className=" ">
               <div
-                className="card text-dark bg-light margin-center"
+                className="card text-dark bg-light mx-auto shadow"
                 key={item.id}
               >
                 <div className="card-header">
                   <p>
-                    <b>{item.username}</b>
+                    <b>{item.login.username}</b>
                   </p>
                 </div>
-                <img className="card-img mh-25" src={photoList.unsplash} />
+                <img className="card-img mh-25" src={item.picture.large} />
 
                 <div className="card-body text-center ">
                   <div className=" text-dark ">
                     <p className="card-text">
-                      <b>Name:</b> {item.name}
+                      <b>Name:</b> {item.name.first}
                     </p>
                     <p>
                       <b>Email:</b> {item.email}
@@ -42,7 +43,7 @@ const Card = () => {
                       <b>Phone:</b> {item.phone}
                     </p>
                     <p>
-                      <b>Website:</b> {item.website}
+                      <b>Website:</b> {item.email}
                     </p>
                     <p>
                       <b>Phone:</b> {item.phone}
